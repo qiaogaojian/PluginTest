cf285712c78b818c61293e9da6edaa55dc5117d4
qiaogaojian <qiaogaojian@vip.qq.com>
2022-06-20 17:44:12 +0800

    feat: hotreload

--- main.ts
+++ main.ts
@@ -19,7 +19,7 @@ export default class MyPlugin extends Plugin {
 		// This creates an icon in the left ribbon.
 		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+			new Notice('Hello You!');
 		});
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
@@ -76,10 +76,12 @@ export default class MyPlugin extends Plugin {
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+        
+        console.log("******************************* onunload *******************************")
 	}
 
 	onunload() {
-
+        console.log("******************************* onunload *******************************")
 	}
 
 	async loadSettings() {
